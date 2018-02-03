9331f473f66136e07d60cf0a50436621bfe1377f
rosarioarobinson <rosario.a.robinson@gmail.com>
2018-02-02 21:27:58 -0500

    Foursquare API

--- js/app.js
+++ js/app.js
@@ -1,82 +1,241 @@
 //Base code help provided by Udacity Javascript Design Patterns Course
-// Model for currentMarkers
+// Model
+// model for currentMarkers
 var currentMarkers = [];
+
+
 
 // Model for Location
 var Location = function(data) {
-    this.clickLocation = ko.observable(data.clickLocation);
-    this.placename = ko.observable(data.placename);
-    this.location = ko.observable(data.location);
-    this.showMarker = ko.observable(true);
-    this.placeMe = ko.observable(true);
+
+    self.placename = ko.observable(data.placename);
+    self.location = ko.observable(data.location);
 };
 
-//ViewModel
+
+
+// ViewModel
 var ViewModel = function() {
 
     var self = this;
 
+    this.filterLocation = ko.observableArray();
+    self.filteredLocations = ko.observableArray();
     this.dcLocations = ko.observableArray(dcLocations);
-
-    // Populate the list on the side
     this.locationList = ko.observableArray([]);
+    self.currentLocation = ko.observable(this.locationList()[0]);
+    self.searchField = ko.observable('');
+    this.showLocation = ko.observable(true);
 
-    dcLocations.forEach(function(locationItem) {
-        self.locationList.push(new Location(locationItem));
-    });
+    // filter dcLocations based on keystroke
+    this.dcLocations = ko.computed(function() {
+        var search = self.searchField().toLowerCase();
 
-    this.currentLocation = ko.observable(this.locationList()[0]);
+        for (var i = 0; i < dcLocations.length; i++) {
+            if (dcLocations[i].placename.toLowerCase().indexOf(search) >= 0) {
+                if (dcLocations[i].marker) {
+                    dcLocations[i].marker.setVisible(true);
+                }
+            } else {
+                if (dcLocations[i].marker) {
+                    dcLocations[i].marker.setVisible(false);
+                } // if
+            } // if-else
+        } // for
 
-    // Get the text from the searchField
-    this.searchField = ko.observable('');
-
-    // Try new things
-    this.dcLocations = ko.dependentObservable(function() {
-        var search = this.searchField().toLowerCase();
         return ko.utils.arrayFilter(dcLocations, function(location) {
             return location.placename.toLowerCase().indexOf(search) >= 0;
         });
-    }, this);
+
+    }, this); 
+
+    // Display the marker on the map based on user click on the location list
+    self.showLocation = function(location) {
+        for (var i = 0; i < dcLocations.length; i++) {
+            //var place = new Location(dcLocations[i]);
+            if (location.placename == dcLocations[i].placename) {
+                dcLocations[i].marker.setVisible(true);
+            } else {
+                dcLocations[i].marker.setVisible(false);
+            }
+
+        }
+        google.maps.event.trigger(location.marker, 'click');
+    }; // self.showLocation
 
 
-    // Incremenet the click number
-    this.incrementCounter = function() {
-        this.clickCount(this.clickCount() + 1);
+    this.setLocation = function(clickedLocation) {
+        self.currentLocation(clickedLocation);
     };
 
-    // Set the location to currentLocation of the list
-    this.setLocation = function(clickLocation) {
-        console.log('hi');
-        self.currentLocation(clickLocation);
-    };
 
-    // To display the marker on the map
-    this.showLocation = function(locations) {
-        console.log('showLocation');
-        console.log('places');
-        google.maps.event.trigger(locations.showMarker, 'click');
-    };
+};
+
+
+// Map function
+var map;
+
+//New array created for listed markers below.
+self.markers = [];
+
+var self = this;
+
+// Add function for foursquare API
+var populateInfoWindow = function(marker, infowindow) {
+
+
+    if (infowindow.marker != marker) {
+
+        infowindow.marker = marker;
+        infowindow.open(map, marker);
+
+
+        //foursquare API
+        var VERSION = '20180228';
+        var CLIENT_SECRET = 'HYCQ4DVRBANWUUPZYIHRLI5APRS1NPC4LK0UO5XKJCUDD0M1';
+        var CLIENT_ID = 'ARTCZT4NMVFUFD1XDZ0WWD5AEVHWECZWVRNRM3C53OXDAFQI';
+        var LL = '38.891055,-77.032704';
+        var FS_URL = URL + CLIENT_ID + CLIENT_SECRET;
+        var venue, address;
+
+        //ajax call for data and info for foursquare
+        $.ajax({
+            url:'https://api.foursquare.com/v2/venues/search',
+            dataType: 'json',
+            data: {
+                limit: '1',
+                ll: '38.891055,-77.032704',
+                client_id: CLIENT_ID,
+                client_secret: CLIENT_SECRET,
+                query: marker.placename,
+                v: '20180228'
+            }
+         }).done(function(response) {
+              //displays info from each location
+              //create variables for content below
+              var address = response.response.venues[0].location.address;
+              var phonenumber = response.response.venues[0].contact.formattedPhone;
+              var twitter = response.response.venues[0].contact.twitter;
+              infowindow.setContent('<div>' + '<h4>' + marker.placename + '</h4>' + address + '<br >' + phonenumber + '<br >' + 'Twitter:  ' + twitter + '</h4>' + '</div>');
+         })
+
+        //error for foursquare
+      .fail(function(e) {
+            alert("Oh No! FourSquare API is currently unavailable. Please try again later.");
+        });
+
+
+    }
+
+    infowindow.addListener('closeclick', function() {
+    });
 
 
 
 };
 
-//alert for Google Error Message
-function googleError() {
-    alert("Google Maps API is currently unavailable. Please try again later.");
+
+    // Alerts for Map Error Messages
+    function googleError() {
+        alert("Google Maps API is currently unavailable. Please try again later.");
+    }
+
+
+// Initiate the map
+function initMap() {
+    // Setup Error messages
+    this.showGoogleMessage = ko.observable(false);
+    this.show4SquareMessage = ko.observable(false);
+    self.locationList = ko.observableArray([]);
+
+    self.map = new google.maps.Map(document.getElementById('map'), {
+        //Location for Washington, D.C.
+        center: {
+            lat: 38.907192,
+            lng: -77.036871
+        },
+        zoom: 13
+    });
+
+    self.setMarker = function(data) {
+        self.locationList().forEach(function(location) {
+            location.marker.setVisible(false);
+        });
+
+        data.marker.setVisible(true);
+
+        //makes markers bounce
+        data.marker.setAnimation(google.maps.Animation.BOUNCE);
+        setTimeout(function() {
+            data.marker.setAnimation(null);
+        }, 2000);
+        map.setCenter(data.marker.position);
+    };
+
+
+    var toggleBounce = function(marker) {
+        if (marker.getAnimation() !== null) {
+            marker.setAnimation(null);
+        } else {
+            marker.setAnimation(google.maps.Animation.BOUNCE);
+            setTimeout(function() {
+                marker.setAnimation(null);
+            }, 1000);
+        }
+    };
+
+
+    function markerClickActions(marker) {
+        marker.addListener('click', function() {
+            populateInfoWindow(this, largeInfoWindow);
+        });
+    }
+
+    this.largeInfoWindow = new google.maps.InfoWindow();
+
+    function markerCall(marker) {
+        var self = this;
+        //self.setAnimation(google.maps.Animation.BOUNCE);
+        this.marker.setAnimation(google.maps.Animation.BOUNCE);
+        setTimeout(function() {
+            self.setAnimation(null);
+        }, 1400);
+        populateInfoWindow(this, largeInfoWindow);
+    }
+
+    //This for loop use the location array to create an array of markers on the map.
+    for (var i = 0; i < dcLocations.length; i++) {
+        var markerPlacename = dcLocations[i].placename;
+        var markerLat = dcLocations[i].lat;
+        var markerLng = dcLocations[i].lng;
+        var marker = new google.maps.Marker({
+            map: map,
+            position: {
+                lat: markerLat,
+                lng: markerLng
+            },
+            placename: markerPlacename,
+            id: 1,
+            animation: google.maps.Animation.DROP
+        });
+        dcLocations[i].marker = marker;
+        //marker.addListener calls to function below to animate and populate infowindow
+        marker.addListener('click', markerClickHandler);
+        markerClickActions(marker);
+    }
+
+    function markerClickHandler() {
+        var self = this;
+        self.setAnimation(google.maps.Animation.BOUNCE);
+        setTimeout(function() {
+            self.setAnimation(null);
+        }, 1400);
+        populateInfoWindow(this, largeInfoWindow);
+    }
+
+
+
+    // applyBindings for ViewModel
+    ko.applyBindings(new ViewModel());
+
 }
-
-function fourSquareError() {
-    alert("Four Square API is currently unavailable. Please try again later.");
-}
-
-
-ko.applyBindings(new ViewModel());
-
-
-$('.hamburger').click(function() {
-    $('.container').toggleClass('open');
-});
-$('.list_element').click(function() {
-    $('.container').toggleClass('open');
-});
